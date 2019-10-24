154ed9ed7c3483463ac72205ad221ef7bd992998
Nikita <minetown2222@gmail.com>
2019-10-24 23:24:39 +0300

    week10

---
+++ week10/ex4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
+ino_t inodes[MAX];
+
+int main(){
+    DIR *dirp = opendir("./tmp");
+    if (dirp == NULL) { 
+        return -1; 
+    }
+    struct dirent *dp;
+    char *names[MAX];
+    int i = 0;
+
+    while((dp = readdir(dirp)) != NULL && i < MAX){
+        inodes[i] = dp->d_ino;
+        names[i] = dp->d_name;
+        printf("%s\n", dp->d_name);
+        ++i;
+    }
+    for(i = 0; i < MAX; ++i){
+        int counter = 1;
+        for(int j = i + 1; j < MAX; ++j){
+            if(inodes[i] == inodes[j] && inodes[j] != 0){
+                if(counter == 1){
+                    printf("Files with i-nodes with hard link count of two or more: %s %s", names[i], names[j]);
+                    ++counter;
+                    inodes[j] = 0;
+                }
+                else{
+                    printf("%s", names[j]);
+                    inodes[j] = 0;
+                }
+            }
+        }
+        inodes[i] = 0;
+        if(counter != 1)
+            printf("\n");
+    }
+    closedir(dirp);
+    return 1;
+}
